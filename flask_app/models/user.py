b931ebe9f2a9a4afc84d1921452766cd4845a853
Arvel Cavitt <arvelcavitt4@gmail.com>
2024-03-08 00:13:20 -0800

    commit

---
+++ flask_app/models/user.py
@@ -0,0 +1,21 @@
+from flask_app import app
+from flask_app.config.mysqlconnection import connectToMySQL
+
+
+
+
+class User:
+    db = "MacroDB"
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        
+    @classmethod
+    def create_user(cls,data):
+        query = "INSERT INTO user (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
+        return connectToMySQL(cls.db).query_db(query, data)
